f3a2bd3b925f232a68079fe565094a063f7b99fc
Hasan Seam <hasanseam37@gmail.com>
2020-03-09 18:58:41 +0100

    updated

--- javascript/fabcarUI/src/app/query/query.component.ts
+++ javascript/fabcarUI/src/app/query/query.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-query',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./query.component.css']
 })
 export class QueryComponent implements OnInit {
-
-  constructor() { }
+  public results:any = [];
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    let obs = this.http.get('http://'+location.hostname+':8080/api/query/');
+    obs.subscribe((response)=> {
+      this.results = response;
+      console.log(this.results);
+    });
   }
 
 }
